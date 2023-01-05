230b0ffa379140950dcf9537c8f7dffef5e0a539
fthiagomedeiros <fthiagomedeiros@gmail.com>
2023-01-05 22:57:28 +0000

    Using some tests tools from the starter project

--- src/test/java/com/example/springtest/SpringTestApplicationTests.java
+++ src/test/java/com/example/springtest/SpringTestApplicationTests.java
@@ -1,8 +1,11 @@
 package com.example.springtest;
 
+import com.jayway.jsonpath.JsonPath;
+import org.json.JSONException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -31,6 +34,31 @@ class SpringTestApplicationTests {
         assertNotEquals("John", "Duke");
         assertThrows(NumberFormatException.class, () -> Integer.valueOf("duke"));
         assertEquals("hello world", "HELLO WORLD".toLowerCase());
+    }
+
+    @Test
+    void jsonAssertExample() throws JSONException {
+        String result = "{\"name\": \"duke\", \"age\":\"42\"}";
+        JSONAssert.assertEquals("{\"name\": \"duke\"}", result, false);
+    }
+
+    //JSONAssert
+    @Test
+    void jsonAssertExample2() throws JSONException {
+
+        //Strict order
+        String result = "{\"name\": \"duke\", \"age\":\"42\"}";
+        JSONAssert.assertEquals("{\"name\": \"duke\"}", result, true);
+    }
+
+    //JSONPath
+    @Test
+    void jsonPathExample() {
+        String result = "{\"age\":\"42\", \"name\": \"duke\", \"tags\":[\"java\", \"jdk\"]}";
+
+        // Using JUnit 5 Assertions
+        assertEquals(2, JsonPath.parse(result).read("$.tags.length()", Long.class));
+        assertEquals("duke", JsonPath.parse(result).read("$.name", String.class));
     }
 
 }
