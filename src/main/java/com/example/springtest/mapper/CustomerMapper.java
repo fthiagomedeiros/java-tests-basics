e6a3258968f49a6f89f40e204b02925abfd54708
Thiago Medeiros <fmedeiros.edge@scml.pt>
2023-03-08 23:59:36 +0000

    Removing unnecessary code from mapstructs

--- src/main/java/com/example/springtest/mapper/CustomerMapper.java
+++ src/main/java/com/example/springtest/mapper/CustomerMapper.java
@@ -4,12 +4,9 @@ import com.example.springtest.domain.Customer;
 import com.example.springtest.domain.CustomerDTO;
 import java.util.List;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface CustomerMapper {
-
-    CustomerMapper MAPPER = Mappers.getMapper(CustomerMapper.class);
 
     CustomerDTO mapToCustomerDTO(Customer customer);
 
